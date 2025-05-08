374bfacd47fb535708eb7ccd57ae79c42b706a5e
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-05-09 00:27:17 +0700

    basic scaffold

--- lib/presentation/TodoPage.dart
+++ lib/presentation/TodoPage.dart
@@ -7,6 +7,11 @@ const TodoPage({ Key? key }) : super(key: key);
   Widget build(BuildContext context){
     final _key = GlobalKey<FormState>();
     final _controller = TextEditingController();
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
   }
 }
