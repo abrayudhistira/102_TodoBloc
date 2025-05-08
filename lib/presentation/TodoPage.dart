6370e7551c156489f59417642c37f032cc3cc26c
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-05-09 00:26:11 +0700

    declaration variabel for key and controller handle

--- lib/presentation/TodoPage.dart
+++ lib/presentation/TodoPage.dart
@@ -5,6 +5,8 @@ const TodoPage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
     return Container();
   }
 }
